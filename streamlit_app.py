d40b53841afe994e0a68266c2790052148aa8b5e
phubertin <phubertin@yahoo.fr>
2023-11-09 10:52:05 -0500

    Calling Fruityvice API from Streamit APP
    🥋 Let's Call the Fruityvice API from Our Streamlit App!

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+##LESSON 9 : 🥋 Let's Call the Fruityvice API from Our Streamlit App!
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
