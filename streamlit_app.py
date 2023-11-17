68864f5bdfa92a61d6d2bfb0abc5b399e1bc32e2
phubertin <phubertin@yahoo.fr>
2023-11-16 21:40:23 -0500

    Update streamlit_app.py
    Minor correction, syntaxe

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 ##LESSON 9 : 🥋 Let's Call the Fruityvice API from Our Streamlit App!
 #create the repeatable code block (called function)
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
@@ -38,7 +38,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please, select a fruit to get information.")
   else:
-    back_from_function = get(fruityvice_data(fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
                              
 ##New section to display Fruityvice API response 
