803263a0d7d3f73ff0e04d8dd47eb74170b78b70
phubertin <phubertin@yahoo.fr>
2023-11-17 10:30:12 -0500

    Update streamlit_app.py (Load into a Button Action)
    🥋 Move the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,23 @@ except URLError as e:
 # my_cur = my_cnx.cursor()
 # my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 # my_data_rows = my_cur.fetchall()
-# streamlit.header("The fruit load list contains :")
 # streamlit.dataframe(my_data_rows)
+
+streamlit.header("The fruit load list contains :")
+#snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+      return my_cur.fetchall()
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+    
+
+
 
 ##🎯 Can You Add A Second Text Entry Box?
 # streamlit.header("Fruityvice Add My Fruit!")
