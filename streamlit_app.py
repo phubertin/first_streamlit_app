a512b66fe825b55ee75989bc4856cfcd3a73245a
phubertin <phubertin@yahoo.fr>
2023-11-16 22:10:28 -0500

    Update streamlit_app.py
    updated except error

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ try:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
 
-## expect URLError as e:
-##  streamlit.error()
+except URLError as e:
+    streamlit.error()
